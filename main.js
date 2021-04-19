8cb71162a0cafaa5550fade3ea76be133bb7e46d
Mrityunjay112358 <73526816+Mrityunjay112358@users.noreply.github.com>
2021-04-19 16:06:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(600,600);
+
+    canvas.position(350,200);
+
+    video=createCapture(VIDEO);
+
+video.hide();
+
+circle1= circle(70,70,70);
+    circle2= circle(530,530,70);
+   circle3=  circle(530,70,70);
+    circle4= circle(70,530,70);
+     rect1 = rect(105,60,390,20);
+    rect2= rect(60,105,20,390);
+     rect3 = rect(520,105,20,390);
+    rect4= rect(105,520,390,20);
+  
+}
+
+function draw(){
+  image(video,200,200,200,200);
+}
+
+
+function take_snapshot(){
+save("selfie.png");
+}
