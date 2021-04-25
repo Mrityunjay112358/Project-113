98c892e00a46627ea836b1b1cdbf8abdc705fbfa
Mrityunjay112358 <73526816+Mrityunjay112358@users.noreply.github.com>
2021-04-25 14:37:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,13 @@ function setup(){
 
 video.hide();
 
-circle1= circle(70,70,70);
+
+  
+}
+
+function draw(){
+  image(video,200,200,200,200);
+  circle1= circle(70,70,70);
     circle2= circle(530,530,70);
    circle3=  circle(530,70,70);
     circle4= circle(70,530,70);
@@ -19,11 +25,10 @@ circle1= circle(70,70,70);
     rect2= rect(60,105,20,390);
      rect3 = rect(520,105,20,390);
     rect4= rect(105,520,390,20);
-  
-}
 
-function draw(){
-  image(video,200,200,200,200);
+    circle1.fill('yellow');
+    
+    
 }
 
 
